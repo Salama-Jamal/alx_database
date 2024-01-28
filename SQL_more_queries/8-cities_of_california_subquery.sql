21858e72b3e41db05b4bc210f8b5fce64b86e693
Jamal Salama <148999243+Salama-Jamal@users.noreply.github.com>
2024-01-28 02:38:01 +0100

    Update 8-cities_of_california_subquery.sql

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,9 +1,9 @@
--- Generating database hbtn_0d_usa and the table states (in the database hbtn_0d_usa)
--- Expalnation of table data
-
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states (
-       id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
-       name VARCHAR(256) NOT NULL
-);
+-- Lists all cities of CA in the database hbtn_0d_usa.
+-- Results are ordered by ascending cities.id.
+SELECT `id`, `name`
+  FROM `cities`
+ WHERE `state_id` IN
+       (SELECT `id`
+	  FROM `states`
+	 WHERE `name` = "California")
+ ORDER BY `id`;
