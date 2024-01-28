db772f5a5389545e2f4ece5a2c114db2612ae206
Jamal Salama <148999243+Salama-Jamal@users.noreply.github.com>
2024-01-28 02:27:22 +0100

    Update 0-privileges.sql

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,12 +1,4 @@
-#!/usr/bin/python3
-
-def square_matrix_simple(matrix=[]):
-    return ([list(map(lambda x: x * x, row)) for row in matrix])
-
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-
-new_matrix = square_matrix_simple(matrix)
+-- Salama-Jamal (0-privileges.sql)
+-- Generating scripts that lists all privileges of the MySQL
+SHOW GRANTS FOR 'user_0d_1 '@' localhost';
+SHOW GRANTS FOR 'user_0d_2 '@' localhost';
